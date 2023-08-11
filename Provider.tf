48635c481d963311417300a4557f5247b1d11232
leelabhavanijaddu <74944599+leelabhavanijaddu@users.noreply.github.com>
2023-08-11 11:21:14 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -7,18 +7,18 @@ terraform {
   }
 }
 provider "azurerm" {
-  subscription_id = "b1627956-95e4-4381-86e1-629f221ce542"
-  client_id       = "b9757333-731c-4772-b24a-302a8739dc66"
-  client_secret   = "NHY8Q~kJyxiy-5jerKTRkzw24BxIDbv6UYsE-csR"
-  tenant_id       = "cfb84c28-63d7-4fe1-91bc-bb133d2076a1"
+  subscription_id = "****************"
+  client_id       = "*******************"
+  client_secret   = "*********************"
+  tenant_id       = "********************"
   features {}
 
 }
 terraform{
   backend "azurerm"{
-    storage_account_name = "leela12345"
-    container_name = "tfstate"
+    storage_account_name = "storageacc"
+    container_name = "container"
     key = "terraform.tfstate"
-    access_key = "oyyw/mn8K7YUbYI5CR6KWb6Bmr/AJsf3AAk06ObBc1kHrT6UxAqB0gf/TMETRgtXSSXCB79h7q83+AStiQhcgw=="
+    access_key = "<access_key_of_storageacc>"
   }
 }
