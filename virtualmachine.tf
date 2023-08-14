b116dcbb7cb96ae7ddd25c632b9923d1ecdc8bc7
leelabhavanijaddu <74944599+leelabhavanijaddu@users.noreply.github.com>
2023-08-14 10:59:30 +0530

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -1,26 +1,26 @@
 resource "azurerm_network_interface" "networkinterface" {
-  name                = "leelanic"
+  name                = "Azurenic"
   location            = local.location
-  resource_group_name = azurerm_resource_group.LeelaRG-1.name
+  resource_group_name = azurerm_resource_group.AzureRG-1.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.leela_public_ip.id
+    public_ip_address_id = azurerm_public_ip.Azure_public_ip.id
   }
 
   depends_on = [ 
     azurerm_virtual_network.virtualnetwork,
-    azurerm_public_ip.leela_public_ip 
+    azurerm_public_ip.Azure_public_ip 
   ]
 }
 
 
 
-resource "azurerm_windows_virtual_machine" "leelavm" {
-  name                = "leelavm"
-  resource_group_name = azurerm_resource_group.LeelaRG-1.name
+resource "azurerm_windows_virtual_machine" "virtualmachine" {
+  name                = "Azurevm"
+  resource_group_name = azurerm_resource_group.AzureRG-1.name
   location            = local.location
   size                = "Standard_F2"
   admin_username      = "TempAdmin"
